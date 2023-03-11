af7c80d77b1bc504df45e7a922eb4255011074f3
frfl61234 <116157780+frfl61234@users.noreply.github.com>
2023-03-11 09:11:05 +0100

    ajout du random pour la baguette il manque celui pour la composition

--- src/main/java/org/example/Core.java
+++ src/main/java/org/example/Core.java
@@ -1,5 +1,8 @@
 package org.example;
 
 public enum Core {
-    PHOENIX_FEATHER, DRAGON_HEARTSTRING;
+    PHOENIX_FEATHER,
+    DRAGON_HEARTSTRING,
+    poile_de_licorne,
+
 }
--- src/main/java/org/example/Wand.java
+++ src/main/java/org/example/Wand.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Wand {
     public Wand ()
     {
-        Scanner scanner = new Scanner (System.in);
 // this
-        System.out.println("Tu dois choisir une baguette, quelle est la taille de votre baguette ?" );
-        int size = scanner.nextInt();
-        System.out.println("Qu'est ce qu'il y a dans ta baguette ? (core)");
-        String core = scanner.next();
-        System.out.println(core);
+        System.out.println("Vous devez avoir une baguette, allez cher Olivender pour en avoir une." );
+        int taille = (int)(Math.random() * 20) ; /* Taille des baguettes entre 16 cm et 35 cm */
+        taille += 16;
+
+        System.out.println("Félicitation votre baguette mesure "+taille+ " cm et elle contient du XXX");
     }
 }
