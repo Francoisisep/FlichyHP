4f26f3c0b0b861f41ad6b55d7daed73412d1f28e
frfl61234 <116157780+frfl61234@users.noreply.github.com>
2023-03-11 09:07:42 +0100

    ajout du switch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         Wizard wizard = new Wizard();
+        Wand joueur = new Wand();
+        System.out.println("À Poudlard vous êtes autorisé d'avoir un animale de compagnie.\n Voulez-vous avoir un animal de compagnie ? soit oui soit non");
+        String réponse = scanner.nextLine();
+        switch (réponse){
+            case "non":
+                System.out.println("Parfait Vous pouvez à Poudlard.");
+                break;
+            case "oui":
+                System.out.println("Vous avez le choix entre: OWL,RAT,CAT,TOAD. Vous devez en choisir 1");
+                /* il manque le enum */
+                System.out.println("Parfait Vous pouvez à Poudlard.");
+                break;
+            default:
+                System.out.println("Vous êtes incapable de répondre correctement donc vous n'aurez pas d'animaux de compagnie.");
+
+        }
+        SortingHat house = new SortingHat();
 
         Pet p1 = Pet.OWL;
         System.out.println(p1);
@@ -13,5 +30,7 @@ public class Main {
         System.out.println(p2);
         Pet p3 = Pet.TOAD;
         System.out.println(p3);
+        Pet p4 = Pet.RAT;
+        System.out.println(p4);
     }
 }
