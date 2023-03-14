546a06b603f44b45d707d1fc0b7ac238d1c9c93d
frfl61234 <116157780+frfl61234@users.noreply.github.com>
2023-03-14 12:36:25 +0100

    Version finale Sortilège

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner scanner = new Scanner (System.in);
         Wizard wizard = new Wizard();
         Wand joueur = new Wand();
+        int niveau = 0;
         System.out.println("À Poudlard vous êtes autorisé d'avoir un animale de compagnie.\n Voulez-vous avoir un animal de compagnie ? soit oui soit non");
         String réponse = scanner.nextLine();
         switch (réponse){
@@ -24,6 +25,29 @@ public class Main {
 
         }
         SortingHat house = new SortingHat();
+        PasserNiveau(niveau);
+        niveau+=1;
+        Niveau1 niveau1 = new Niveau1();
+        PasserNiveau(niveau);
+        niveau+=1;
+        //Niveau2 niveau2 = new Niveau2();
+        PasserNiveau(niveau);
+        niveau+=1;
+        //Niveau3 niveau3 = new Niveau3();
+        PasserNiveau(niveau);
+        niveau+=1;
+        //Niveau4 niveau4 = new Niveau4();
+        PasserNiveau(niveau);
+        niveau+=1;
+        //Niveau5 niveau5 = new Niveau5();
+        PasserNiveau(niveau);
+        //Niveau6 niveau6 = new Niveau6();
+        //Niveau7 niveau7 = new Niveau7();
+
+
+
+        }
+
 /*
         Pet p1 = Pet.OWL;
         System.out.println(p1);
@@ -33,5 +57,11 @@ public class Main {
         System.out.println(p3);
         Pet p4 = Pet.RAT;
         System.out.println(p4);*/
-    }
+public static void PasserNiveau(int niveau) {
+    System.out.println("Vous avez un cours de sortilège");
+    Sortilèges sort = new  Sortilèges(niveau);
+
 }
+}
+
+
