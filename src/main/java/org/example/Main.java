3570561c3412aeaefc4ac3c2820165bf9d6bd0a6
frfl61234 <116157780+frfl61234@users.noreply.github.com>
2023-03-14 13:01:04 +0100

    modification

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,41 +4,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         Wizard wizard = new Wizard();
         Wand joueur = new Wand();
         int niveau = 0;
         System.out.println("À Poudlard vous êtes autorisé d'avoir un animale de compagnie.\n Voulez-vous avoir un animal de compagnie ? soit oui soit non");
         String réponse = scanner.nextLine();
-        switch (réponse){
-            case "non" :
-                System.out.println("Parfait Vous pouvez à Poudlard.");
-                break;
+        switch (réponse) {
             case "oui":
                 System.out.println("Vous avez le choix entre: OWL,RAT,CAT,TOAD. Vous devez en choisir 1");
                 /* il manque le enum */
+                String animal = scanner.nextLine(); // demander l'annimal
                 System.out.println("Parfait Vous pouvez à Poudlard.");
                 break;
-
             default:
-                System.out.println("Vous êtes incapable de répondre correctement donc vous n'aurez pas d'animaux de compagnie.");
+                System.out.println("Parfait Vous pouvez à Poudlard.");
+                break;
 
         }
         SortingHat house = new SortingHat();
         PasserNiveau(niveau);
-        niveau+=1;
+        niveau += 1;
         Niveau1 niveau1 = new Niveau1();
         PasserNiveau(niveau);
-        niveau+=1;
+        niveau += 1;
         //Niveau2 niveau2 = new Niveau2();
         PasserNiveau(niveau);
-        niveau+=1;
+        niveau += 1;
         //Niveau3 niveau3 = new Niveau3();
         PasserNiveau(niveau);
-        niveau+=1;
+        niveau += 1;
         //Niveau4 niveau4 = new Niveau4();
         PasserNiveau(niveau);
-        niveau+=1;
+        niveau += 1;
         //Niveau5 niveau5 = new Niveau5();
         PasserNiveau(niveau);
         //Niveau6 niveau6 = new Niveau6();
@@ -46,17 +44,19 @@ public class Main {
 
 
 
-        }
 
 /*
-        Pet p1 = Pet.OWL;
-        System.out.println(p1);
-        Pet p2 = Pet.CAT;
-        System.out.println(p2);
-        Pet p3 = Pet.TOAD;
-        System.out.println(p3);
-        Pet p4 = Pet.RAT;
-        System.out.println(p4);*/
+    Pet p1 = Pet.OWL;
+       // System.out.println(p1);
+    Pet p2 = Pet.CAT;
+        //System.out.println(p2);
+    Pet p3 = Pet.TOAD;
+     //   System.out.println(p3);
+    Pet p4 = Pet.RAT;
+        System.out.println(p1+" "+p2+" "+p3+" "+p4);
+
+ */
+}
 public static void PasserNiveau(int niveau) {
     System.out.println("Vous avez un cours de sortilège");
     Sortilèges sort = new  Sortilèges(niveau);
